57fd4ba0150232d11cea324917b3cd4b7a8b2ca5
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2022-12-26 16:22:36 +0500

    2 part 19 lesson

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['tsconfig.json']
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-uses-react': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/prefer-nullish-coalescing': 0,
-    '@typescript-eslint/strict-boolean-expressions': 0,
-    '@typescript-eslint/naming-convention': 1,
-    '@typescript-eslint/no-floating-promises': 1,
-    '@typescript-eslint/no-misused-promises': 1
-  }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+    overrides: [{
+        files: ['*.ts', '*.tsx'],
+        extends: [
+            'plugin:react/recommended',
+            'standard-with-typescript',
+            'plugin:i18next/recommended'
+        ],
+
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            project: ['./tsconfig.json'],
+        },
+
+        rules: {
+            'react/react-in-jsx-scope': 0,
+            'react/jsx-uses-react': 0,
+            '@typescript-eslint/explicit-function-return-type': 0,
+            '@typescript-eslint/prefer-nullish-coalescing': 0,
+            '@typescript-eslint/strict-boolean-expressions': 0,
+            '@typescript-eslint/naming-convention': 1,
+            '@typescript-eslint/no-floating-promises': 1,
+            '@typescript-eslint/no-misused-promises': 1,
+            'i18next/no-literal-string': ['error', {markupOnly: true}],
+        }
+    },],
+
+
 }
